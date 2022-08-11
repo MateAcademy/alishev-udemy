5ad80794d72189a6b583524cd91296bdf34adb3c
Sergey Klunnyi <s.klunniy@gmail.com>
2022-08-11 16:36:00 +0300

    add printWriter

--- src/main/java/io/ex01/ScannerConsoleHandler.java
+++ src/main/java/io/ex01/ScannerConsoleHandler.java
@@ -10,27 +10,30 @@ public class ScannerConsoleHandler {
         String separator = File.separator;
         String path = "/home/user/Documents/Hillel/alishev/log.txt";
 
-        File file = new File(path);
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                String str = scanner.nextLine();
-                System.out.println(str);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        File file = new File(path);
+//        try (Scanner scanner = new Scanner(file)) {
+//            while (scanner.hasNextLine()) {
+//                String str = scanner.nextLine();
+//                System.out.println(str);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
         writeToFile();
     }
 
 
     private static void writeToFile() {
-        String path = "/home/user/Documents/Hillel/alishev/newFile";
+        String path = "/home/user/Documents/Hillel/alishev/newFile.txt";
         File file = new File(path);
 
-        try (PrintWriter pw = new PrintWriter(file)) {
-            pw.println("Test row 1");
-            pw.println("Test row 9");
+        try  {
+            PrintWriter pw = new PrintWriter(file);
+            pw.write("Test row 1");
+            pw.write("Test row 7");
+
+           pw.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
